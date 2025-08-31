cb940f9daf9e3bf246ab3c03f24c863899fdfa66
Tamon-James <unitamon@gmail.com>
2025-09-01 01:43:16 +0900

    login時のcssを作成、reportとnewsの一覧にマウスを合わせたときのアニメーションを追加

--- public/admin_directory/login.php
+++ public/admin_directory/login.php
@@ -20,11 +20,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>ログイン</title>
+    <link rel="stylesheet" href="/css/common.css">
+    <link rel="stylesheet" href="admin_css/admin.css">
 </head>
 <body>
     <h1>管理者ログイン</h1>
-    <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
-    <form method="post">
+    <?php if (!empty($error)) echo "<p class='error'>$error</p>"; ?>
+    <form method="post" class="login-form">
         ユーザー名: <input type="text" name="username"><br>
         パスワード: <input type="password" name="password"><br>
         <input type="submit" value="ログイン">
